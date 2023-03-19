696650959057cd13e0ecb8627d0106aa798e2ad1
viktorykings <kingsviktory@gmail.com>
2023-03-20 00:33:03 +0300

    feat: add more tests

--- src/tests/Main.test.tsx
+++ src/tests/Main.test.tsx
@@ -1,12 +1,11 @@
-import { describe, test, expect } from 'vitest'
-import { render, fireEvent } from '@testing-library/react'
+import { describe, test, expect } from 'vitest';
+import { render } from '@testing-library/react';
 import React from 'react';
 import Main from '../components/Main';
 
 describe('<Main />', () => {
-      test('Main mounts properly', () => {
-    const wrapper = render(<Main />)
-    expect(wrapper).toBeTruthy()
-  })
+  test('Main mounts properly', () => {
+    const wrapper = render(<Main />);
+    expect(wrapper).toBeTruthy();
+  });
 });
-
