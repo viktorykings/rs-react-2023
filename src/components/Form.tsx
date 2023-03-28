1b0e14ece55edaf7dcadbe1641a26a1138e82352
viktorykings <kingsviktory@gmail.com>
2023-03-28 23:54:08 +0300

    feat: rewrite form with reakt forms hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,126 +2,48 @@ import React, { FormEvent } from 'react';
 import SuccessModal from './SuccessModal';
 import { FormState, FormSetState, FormData } from './types';
 import { chooseSex, genStrings, genBooleans, validateFormData } from './helpers';
+import { useForm, SubmitHandler } from "react-hook-form";
 
-export default class Form extends React.Component<FormSetState, FormState> {
-  name: React.RefObject<HTMLInputElement>;
-  surname: React.RefObject<HTMLInputElement>;
-  birthDate: React.RefObject<HTMLInputElement>;
-  isBirthDateVis: React.RefObject<HTMLInputElement>;
-  region: React.RefObject<HTMLSelectElement>;
-  male: React.RefObject<HTMLInputElement>;
-  female: React.RefObject<HTMLInputElement>;
-  other: React.RefObject<HTMLInputElement>;
-  profilePic: React.RefObject<HTMLInputElement>;
-  personalData: React.RefObject<HTMLInputElement>;
-  form: React.RefObject<HTMLFormElement>;
-  constructor(props: FormSetState) {
-    super(props);
-    this.state = {
-      data: [],
-      errors: {},
-      saved: false,
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.name = React.createRef();
-    this.surname = React.createRef();
-    this.birthDate = React.createRef();
-    this.isBirthDateVis = React.createRef();
-    this.region = React.createRef();
-    this.male = React.createRef();
-    this.female = React.createRef();
-    this.other = React.createRef();
-    this.profilePic = React.createRef();
-    this.personalData = React.createRef();
-    this.form = React.createRef();
-  }
+type Inputs = {
+  example: string,
+  exampleRequired: string,
+};
+const Form = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>();
+  const onSubmit: SubmitHandler<FormData> = data => console.log(data);
 
-  handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    const str = genStrings(
-      this.name.current!.value,
-      this.surname.current!.value,
-      this.birthDate.current!.value,
-      this.region.current!.value,
-      this.profilePic.current!.value
-    );
-    const boo = genBooleans(
-      this.isBirthDateVis.current!.checked,
-      this.male.current!.checked,
-      this.female.current!.checked,
-      this.other.current!.checked,
-      this.personalData.current!.checked
-    );
-    const sex = chooseSex(boo!.male!, boo!.female!, boo!.other!);
-    const newCard: FormData = {
-      sex: undefined,
-      name: undefined,
-      surname: undefined,
-      birthDate: undefined,
-      region: undefined,
-      profilePic: undefined,
-      isBirthDateVis: undefined,
-      male: undefined,
-      female: undefined,
-      other: undefined,
-      personalData: undefined,
-    };
-    Object.assign(newCard, str, boo, { sex: sex });
-    if (newCard) {
-      console.log(newCard);
-
-      const resObj = validateFormData(newCard);
-      if (resObj.isValid) {
-        console.log('card created');
-        this.props.createCard(newCard);
-        this.clearForm();
-        this.setState({ saved: true });
-        setTimeout(() => {
-          this.setState({ saved: false });
-        }, 3000);
-      } else {
-        this.setState({ errors: resObj.errors });
-      }
-    }
-  }
-
-  clearForm() {
-    this.form.current?.reset();
-    this.setState({ errors: {} });
-  }
-
-  render() {
-    return (
+  console.log(watch("name"))
+  return (
       <div className="form-container">
-        <form action="" className="form" onSubmit={this.handleSubmit} ref={this.form}>
+        <form action="" className="form" onSubmit={handleSubmit(onSubmit)} {...register}>
           <label>
             Name
             <div>
-              <input type="text" ref={this.name} />
-              {this.state.errors?.name && <p className="error">{this.state.errors.name}</p>}
+              <input type="text" {...register('name')} />
+              {errors.name && <p className="error">this f is req</p>}
             </div>
           </label>
           <label>
             Surname
             <div>
-              <input type="text" ref={this.surname} />
-              {this.state.errors?.surname && <p className="error">{this.state.errors.surname}</p>}
+              <input type="text" {...register('surname')} />
+              {errors.surname && <p className="error">this f is req</p>}
             </div>
           </label>
           <label>
             Date of birth
             <div>
-              <input type="date" name="" id="" ref={this.birthDate} />
-              {<p className="error">{this.state.errors.birthDate}</p>}
+              <input type="date" {...register('birthDate')} />
+              {errors.birthDate && <p className="error">this f is req</p>}
             </div>
           </label>
           <label className="checkbox-input">
-            Show birth date in profile <input type="checkbox" ref={this.isBirthDateVis} />
+            Show birth date in profile <input type="checkbox" {...register('isBirthDateVis')} />
           </label>
           <label className="select-input">
             Region
             <div>
-              <select name="selectedRegion" ref={this.region}>
+              <select {...register('region')}>
                 <option value="Europe">Europe</option>
                 <option value="North America">North America</option>
                 <option value="South America">South America</option>
@@ -130,40 +52,204 @@ export default class Form extends React.Component<FormSetState, FormState> {
                 <option value="Australia">Australia</option>
                 <option value="Antarctica">Antarctica</option>
               </select>
-              {this.state.errors?.region && <p className="error">{this.state.errors.region}</p>}
+              {errors.region && <p className="error">this f is req</p>}
             </div>
           </label>
           <label className="radio-input">
             <div className="radio-input-container">
               <label htmlFor="male">
-                Male <input type="radio" name="sex" id="male" ref={this.male} />
+                Male <input type="radio" id="male" {...register('male')} />
               </label>
               <label htmlFor="female">
-                Female <input type="radio" name="sex" id="female" ref={this.female} />
+                Female <input type="radio" id="female" {...register('female')} />
               </label>
               <label htmlFor="other">
-                Other <input type="radio" name="sex" id="other" ref={this.other} />
+                Other <input type="radio" id="other" {...register('other')} />
               </label>
             </div>
-            {<p className="error">{this.state.errors.sex}</p>}
+            {errors.sex && <p className="error">req</p>}
           </label>
           <label>
             Add profile picture
             <div>
-              <input type="file" name="profile-picture" id="profilePicture" ref={this.profilePic} />
-              {<p className="error">{this.state.errors.profilePic}</p>}
+              <input type="file" id="profilePicture" {...register('profilePic')} />
+              {errors.profilePic && <p className="error">req</p>}
             </div>
           </label>
           <div className="terms">
             <label className="checkbox-input">
-              I consent to my personal data <input type="checkbox" ref={this.personalData} />
+              I consent to my personal data <input type="checkbox" {...register('personalData')} />
             </label>
-            {<p className="error">{this.state.errors.personalData}</p>}
+            {errors.personalData && <p className="error">req</p>}
           </div>
           <button type="submit">Create</button>
         </form>
-        <SuccessModal saved={this.state.saved} />
+        {/* <SuccessModal saved={...register(state.saved)} /> */}
       </div>
-    );
-  }
+)
 }
+export default Form;
+// export default class Form extends React.Component<FormSetState, FormState> {
+//   name: React.bject<HTMLInputElement>;
+//   surname: React.bject<HTMLInputElement>;
+//   birthDate: React.bject<HTMLInputElement>;
+//   isBirthDateVis: React.bject<HTMLInputElement>;
+//   region: React.bject<HTMLSelectElement>;
+//   male: React.bject<HTMLInputElement>;
+//   female: React.bject<HTMLInputElement>;
+//   other: React.bject<HTMLInputElement>;
+//   profilePic: React.bject<HTMLInputElement>;
+//   personalData: React.bject<HTMLInputElement>;
+//   form: React.bject<HTMLFormElement>;
+//   constructor(props: FormSetState) {
+//     super(props);
+//     ...register(state =) {
+//       data: [],
+//       errors: {},
+//       saved: false,
+//     };
+//     ...register(handleS)ubmit = ...register(handleS)ubmit.bind(...register);
+//     ...register(name = )React.create);
+//     ...register(surname) = React.create);
+//     ...register(birthDa)te = React.create);
+//     ...register(isBirth)DateVis = React.create);
+//     ...register(region )= React.create);
+//     ...register(male = )React.create);
+//     ...register(female )= React.create);
+//     ...register(other =) React.create);
+//     ...register(profile)Pic = React.create);
+//     ...register(persona)lData = React.create);
+//     ...register(form = )React.create);
+//   }
+
+//   handleSubmit(e: FormEvent) {
+//     e.preventDefault();
+//     const str = genStrings(
+//       ...register(name.cu)rrent!.value,
+//       ...register(surname).current!.value,
+//       ...register(birthDa)te.current!.value,
+//       ...register(region.)current!.value,
+//       ...register(profile)Pic.current!.value
+//     );
+//     const boo = genBooleans(
+//       ...register(isBirth)DateVis.current!.checked,
+//       ...register(male.cu)rrent!.checked,
+//       ...register(female.)current!.checked,
+//       ...register(other.c)urrent!.checked,
+//       ...register(persona)lData.current!.checked
+//     );
+//     const sex = chooseSex(boo!.male!, boo!.female!, boo!.other!);
+//     const newCard: FormData = {
+//       sex: undefined,
+//       name: undefined,
+//       surname: undefined,
+//       birthDate: undefined,
+//       region: undefined,
+//       profilePic: undefined,
+//       isBirthDateVis: undefined,
+//       male: undefined,
+//       female: undefined,
+//       other: undefined,
+//       personalData: undefined,
+//     };
+//     Object.assign(newCard, str, boo, { sex: sex });
+//     if (newCard) {
+//       console.log(newCard);
+
+//       const resObj = validateFormData(newCard);
+//       if (resObj.isValid) {
+//         console.log('card created');
+//         ...register(props.c)reateCard(newCard);
+//         ...register(clearFo)rm();
+//         ...register(setStat)e({ saved: true });
+//         setTimeout(() => {
+//           ...register(setStat)e({ saved: false });
+//         }, 3000);
+//       } else {
+//         ...register(setStat)e({ errors: resObj.errors });
+//       }
+//     }
+//   }
+
+//   clearForm() {
+//     ...register(form.cu)rrent?.reset();
+//     ...register(setStat)e({ errors: {} });
+//   }
+
+//   render() {
+//     return (
+//       <div className="form-container">
+//         <form action="" className="form" onSubmit={...register(handleS)ubmit} {...register(form}>
+//           <label>
+//             Name
+//             <div>
+//               <input type="text" {...register(name} /)>
+//               {errors?.name && <p className="error">{errors.name}</p>}
+//             </div>
+//           </label>
+//           <label>
+//             Surname
+//             <div>
+//               <input type="text" {...register(surname)} />
+//               {errors?.surname && <p className="error">{errors.surname}</p>}
+//             </div>
+//           </label>
+//           <label>
+//             Date of birth
+//             <div>
+//               <input type="date" name="" id="" {...register(birthDa)te} />
+//               {<p className="error">{errors.birthDate}</p>}
+//             </div>
+//           </label>
+//           <label className="checkbox-input">
+//             Show birth date in profile <input type="checkbox" {...register(isBirth)DateVis} />
+//           </label>
+//           <label className="select-input">
+//             Region
+//             <div>
+//               <select name="selectedRegion" {...register(region})>
+//                 <option value="Europe">Europe</option>
+//                 <option value="North America">North America</option>
+//                 <option value="South America">South America</option>
+//                 <option value="Asia">Asia</option>
+//                 <option value="Africa">Africa</option>
+//                 <option value="Australia">Australia</option>
+//                 <option value="Antarctica">Antarctica</option>
+//               </select>
+//               {errors?.region && <p className="error">{errors.region}</p>}
+//             </div>
+//           </label>
+//           <label className="radio-input">
+//             <div className="radio-input-container">
+//               <label htmlFor="male">
+//                 Male <input type="radio" id="male" {...register(male} /)>
+//               </label>
+//               <label htmlFor="female">
+//                 Female <input type="radio" id="female" {...register(female}) />
+//               </label>
+//               <label htmlFor="other">
+//                 Other <input type="radio" id="other" {...register(other} )/>
+//               </label>
+//             </div>
+//             {<p className="error">{errors.sex}</p>}
+//           </label>
+//           <label>
+//             Add profile picture
+//             <div>
+//               <input type="file" name="profile-picture" id="profilePicture" {...register(profile)Pic} />
+//               {<p className="error">{errors.profilePic}</p>}
+//             </div>
+//           </label>
+//           <div className="terms">
+//             <label className="checkbox-input">
+//               I consent to my personal data <input type="checkbox" {...register(persona)lData} />
+//             </label>
+//             {<p className="error">{errors.personalData}</p>}
+//           </div>
+//           <button type="submit">Create</button>
+//         </form>
+//         <SuccessModal saved={...register(state.s)aved} />
+//       </div>
+//     );
+//   }
+// }
