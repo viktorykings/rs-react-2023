5789a84065ad56a0747ac210ca6abf75982ce064
viktorykings <kingsviktory@gmail.com>
2023-04-03 14:00:09 +0300

    fix: add local data for cards, search bar with hooks

---
+++ src/components/data.ts
@@ -0,0 +1,63 @@
+const data = [
+  {
+    name: 'Stephon',
+    surname: 'Graham',
+    birthDate: '1947-05-01T11:53:43.252Z',
+    region: 'Western Sahara',
+    profilePic:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/877.jpg',
+    sex: 'male',
+    personalData: true,
+    isBirthDateVis: false,
+    id: '1',
+  },
+  {
+    name: 'Darrin',
+    surname: 'Jenkins',
+    birthDate: '1987-07-13T17:47:36.131Z',
+    region: 'Croatia',
+    profilePic:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/534.jpg',
+    sex: 'female',
+    personalData: false,
+    isBirthDateVis: true,
+    id: '2',
+  },
+  {
+    name: 'Molly',
+    surname: 'Jerde',
+    birthDate: '2004-01-07T18:29:52.833Z',
+    region: 'Andorra',
+    profilePic:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/74.jpg',
+    sex: 'female',
+    personalData: false,
+    isBirthDateVis: true,
+    id: '3',
+  },
+  {
+    name: 'Abbigail',
+    surname: 'Mante',
+    birthDate: '1972-06-03T02:51:31.826Z',
+    region: 'Maldives',
+    profilePic:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/912.jpg',
+    sex: 'female',
+    personalData: true,
+    isBirthDateVis: true,
+    id: '4',
+  },
+  {
+    name: 'Madelynn',
+    surname: 'Dicki',
+    birthDate: '1969-01-20T06:17:13.043Z',
+    region: 'Pakistan',
+    profilePic:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/71.jpg',
+    sex: 'male',
+    personalData: true,
+    isBirthDateVis: true,
+    id: '5',
+  },
+];
+export default data;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,25 +2,12 @@ import CardsContainer from '../components/CardsContainer';
 import React, { useEffect, useState } from 'react';
 import Search from '../components/Search';
 import { FormData } from '../components/types';
+import data from '../components/data';
 
 const Main = () => {
   const [cards, setCards] = useState<FormData[]>([]);
   useEffect(() => {
-    fetch('https://64158b1d8b5d06e4a7b12b04.mockapi.io/cards/venicles', {
-      method: 'GET',
-      headers: { 'content-type': 'application/json' },
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then((data) => {
-        setCards(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    setCards(data);
   }, [cards]);
   return (
     <main className="main">
